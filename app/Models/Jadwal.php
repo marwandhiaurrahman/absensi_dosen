c574bf80989556068b1f6bac6477aebacf82419a
Marwan Dhiaur Rahman <45331803+marwandhiaurrahman@users.noreply.github.com>
2021-03-14 18:33:36 +0700

    perbaikan jadwal

--- app/Models/Jadwal.php
+++ app/Models/Jadwal.php
@@ -11,4 +11,14 @@ class Jadwal extends Model
     protected $fillable = [
         'hari','jam','kelas_id','matkul_id','ruangan_id',
     ];
+
+    /**
+     * Get the matkul that owns the Jadwal
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function matkul()
+    {
+        return $this->belongsTo(Matkul::class);
+    }
 }
