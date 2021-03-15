33df214360137dd3a65b99f1935600f4236aa6b7
Marwan Dhiaur Rahman <45331803+marwandhiaurrahman@users.noreply.github.com>
2021-03-15 21:27:22 +0700

    penambahan lokasi

--- resources/views/vendor/adminlte/partials/navbar/navbar.blade.php
+++ resources/views/vendor/adminlte/partials/navbar/navbar.blade.php
@@ -12,38 +12,6 @@
 
         {{-- Custom left links --}}
         @yield('content_top_nav_left')
-
-        <li class="nav-item">
-            <a class="nav-link">
-                <div class="row">
-
-                    <!-- Menampilkan Hari, Bulan dan Tahun -->
-                    <script type='text/javascript'>
-                        <!--
-                        var months = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
-var myDays = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jum&#39;at', 'Sabtu'];
-var date = new Date();
-var day = date.getDate();
-var month = date.getMonth();
-var thisDay = date.getDay(),
-thisDay = myDays[thisDay];
-var yy = date.getYear();
-var year = (yy < 1000) ? yy + 1900 : yy;
-document.write(thisDay + ', ' + day + ' ' + months[month] + ' ' + year);
-//
-                        -->
-                    </script>
-                    &emsp;
-                    <!-- Menampilkan Jam (Aktif) -->
-                    <p>Jam :&nbsp;</p>
-                    <div id="clock">00:00:00 </div>&nbsp;WIB
-                    &emsp;
-
-                    <p id="location"></p>
-
-                </div>
-            </a>
-        </li>
     </ul>
 
     {{-- Navbar right links --}}
--- resources/views/vendor/adminlte/partials/sidebar/left-sidebar.blade.php
+++ resources/views/vendor/adminlte/partials/sidebar/left-sidebar.blade.php
@@ -9,6 +9,13 @@
 
     {{-- Sidebar menu --}}
     <div class="sidebar">
+        <div class="user-panel mt-2 pb-3 mb-3 d-flex">
+            <div class="info">
+              <a class="d-block"><div class="d-block" id="tanggal">Tanggal Hari Ini </div></a>
+              <a class="d-block"><div class="d-block" id="clock">Jam : 00:00:00 WIB </div></a>
+              <a class="d-block"><div class="d-block" id="location">Kordinat Lokasi Anda :</div></a>
+            </div>
+          </div>
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column {{ config('adminlte.classes_sidebar_nav', '') }}"
                 data-widget="treeview" role="menu"
@@ -23,5 +30,55 @@
             </ul>
         </nav>
     </div>
+
+
+<script type="text/javascript">
+    function showTime() {
+        var a_p = "";
+        var today = new Date();
+        var curr_hour = today.getHours();
+        var curr_minute = today.getMinutes();
+        var curr_second = today.getSeconds();
+        if (curr_hour == 0) {
+        curr_hour = 12;
+        }
+curr_hour = checkTime(curr_hour);
+curr_minute = checkTime(curr_minute);
+curr_second = checkTime(curr_second);
+document.getElementById('clock').innerHTML="Jam : "+curr_hour + ":" + curr_minute + ":" + curr_second + " WIB";
+}
+function checkTime(i) {
+if (i < 10) {
+i = "0" + i;
+}
+return i;
+}
+setInterval(showTime, 500);
+
+var months = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
+var myDays = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jum&#39;at', 'Sabtu'];
+var date = new Date();
+var day = date.getDate();
+var month = date.getMonth();
+var thisDay = date.getDay(),
+thisDay = myDays[thisDay];
+var yy = date.getYear();
+var year = (yy < 1000) ? yy + 1900 : yy;
+document.getElementById('tanggal').innerHTML = thisDay + ', ' + day + ' ' + months[month] + ' ' + year;
+
+var x = document.getElementById("location");
+function getLocation() {
+if (navigator.geolocation) {
+navigator.geolocation.getCurrentPosition(showPosition);
+} else {
+x.innerHTML = "Geolocation is not supported by this browser.";
+}
+}
+
+function showPosition(position) {
+x.innerHTML = "Koodinat Lokasi Anda : <br>( " + position.coords.latitude +" , " + position.coords.longitude+" )";
+}
+setInterval(getLocation, 500);
+</script>
 
 </aside>
